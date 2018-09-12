e6a3ca36cbb0ab5ab1b6251cd312fc4be53e29cc
Florian Nowarre <f.nowarre@commehr.de>
2018-09-12 11:08:39 +0200

    finisher move

--- Berechtigungen_auslesen2.ps1
+++ Berechtigungen_auslesen2.ps1
@@ -1,6 +1,21 @@
+################################################################################
+<#
+Authors: Florian Nowarre 
+Title: <show-folder-permissions>
+Description / purpose:
+- This script show´s folder permissions or export the inforamtions into a csv
+
+Parameters:
+- all parameters with a Read-Host and a description
+#> 
+#################################################################################
+
+
+
+#variable for the folderpath
 
 Function folder() {
-    $folder = Read-Host 'Type-in the folderpath for example D:\Share\' 
+    $folder = Read-Host 'Type-in the folderpath for ex. D:\Share\' 
 
     Return $folder
 }
@@ -12,8 +27,12 @@ While($folder.length -eq 0) {
     $folder = folder
 }
 
-$csvexport = Read-Host 'Type-in the path for the csv. when you type nothing it will show on the screen' 
+# variable the export path
 
+$csvexport = Read-Host 'Type-in the path for the csv. when you type nothing in, it will shown on the screen' 
+
+
+#Foreach to create the table on the screen or to the csv. columns: Folder, AD Group, Premissions, Inherited 
 
 $FolderPath = dir -Directory -Path "$folder" -Recurse -Force -ErrorAction SilentlyContinue
 $Report = @()
@@ -29,4 +48,4 @@ If($csvexport.length -eq 0) {
     $Report | Format-Table -autosize 
 } Else {
     $Report | Export-Csv -path "$csvexport" -NoTypeInformation 
-}
+}   
